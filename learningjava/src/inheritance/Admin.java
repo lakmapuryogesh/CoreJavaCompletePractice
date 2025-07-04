c2f3443d9fa4b83fe5321922e2ca4b1c04e15b86
lakmapuryogesh <lakmapuryogesh@gmail.com>
2025-07-04 14:45:01 +0530

    super keyword

--- learningjava/src/inheritance/Admin.java
+++ learningjava/src/inheritance/Admin.java
@@ -3,7 +3,14 @@ package inheritance;
 public class Admin extends Developer{
     public void manage() {
     	System.out.println("Manage code");
+    	read();
     }
-	
-
+	/*
+	 * if we use a method name inside othermethod even there
+	 *  is super calss method name it will consider this method 
+	 *  only which is peresent inside the same class
+	 */
+    public void read() {
+    	System.out.println("Admin read code");
+    }
 }
