c34b93381929f1c30468e323fc591d563aeb5877
lakmapuryogesh <lakmapuryogesh@gmail.com>
2025-07-06 18:59:49 +0530

    Encapsulation

--- learningjava/src/encapsulation/Student.java
+++ learningjava/src/encapsulation/Student.java
@@ -8,4 +8,12 @@ public class Student {
       public Student(int rollNumber) {
     	  this.rollNumber=rollNumber;
       }
+      public void setStudentAttendece(boolean flag) {
+    	  isAttendence=flag;
+    	  System.out.println("teacher assigned attendence to student");
+      }
+      public boolean getStudentAttendence() {
+    	  System.out.println("Teacher accessed student attendence");
+    	  return isAttendence;
+      }
 }
