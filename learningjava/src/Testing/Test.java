dc04ff02c0f7754f611fe79c86beee8ff9a92f52
lakmapuryogesh <lakmapuryogesh@gmail.com>
2025-07-10 12:31:05 +0530

    ArrayList

---
+++ learningjava/src/Testing/Test.java
@@ -0,0 +1,17 @@
+package Testing;
+import java.util.List;
+import java.util.ArrayList;
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub'
+		List<String> arr=new ArrayList<>();
+		int[] arr1= {1,2,3};
+		for(int i=0;i<arr1.length;i++) {
+			System.out.println(arr1[i]);
+		}
+		
+
+	}
+
+}
